817178f48bf94312563949b7accff88bd6768908
jhalfs <juebobert@gmail.com>
2017-04-13 15:10:33 -0600

    Update pelicaniformes.pl

--- pelicaniformes.pl
+++ pelicaniformes.pl
@@ -135,91 +135,90 @@ rangesTo(butorides_virescens, canada).
 %Habitat information
 %Possible habitats: lakePond, ocean,  marsh. 
 habitat(pelecanus_erythrorhynchos,	  lakePond).
-habitat(pelecanus_occidentalis,			  ocean).
-habitat(botaurus_lentiginosus,			  marsh).
-habitat(ixobrychus_exilis,				    marsh).
-habitat(ardea_herodias,					      marsh).
-habitat(ardea_alba,						        marsh).
-habitat(egretta_thula,					      marsh).
-habitat(egretta_caerulea,				      marsh).
-habitat(egretta_tricolor,				      marsh).
-habitat(egretta_rufescens,				    marsh).
-habitat(bubulcus_ibis,					      marsh).
-habitat(butorides_virescens,			    marsh).
-habitat(nycticorax_nycticorax,		   	marsh).
-habitat(nyctanassa_violacea,			    marsh).
-habitat(eudocimus_albus,				      marsh).
-habitat(plegadis_falcinellus,			    marsh).
-habitat(plegadis_chihi,					      marsh).
-habitat(platalea_ajaja,					      marsh).
+habitat(pelecanus_occidentalis,		  ocean).
+habitat(botaurus_lentiginosus,		  marsh).
+habitat(ixobrychus_exilis,	    	  marsh).
+habitat(ardea_herodias,			  marsh).
+habitat(ardea_alba,    		          marsh).
+habitat(egretta_thula,		          marsh).
+habitat(egretta_caerulea,	          marsh).
+habitat(egretta_tricolor,	          marsh).
+habitat(egretta_rufescens,	          marsh).
+habitat(bubulcus_ibis,		          marsh).
+habitat(butorides_virescens,	          marsh).
+habitat(nycticorax_nycticorax,	  	  marsh).
+habitat(nyctanassa_violacea,		  marsh).
+habitat(eudocimus_albus,		  marsh).
+habitat(plegadis_falcinellus,		  marsh).
+habitat(plegadis_chihi,		          marsh).
+habitat(platalea_ajaja,		          marsh).
 
 
 %Food information
 %Possible foods: fish, insects
-food(pelecanus_erythrorhynchos,			fish).
-food(pelecanus_occidentalis,			  fish).
-food(botaurus_lentiginosus,				  fish).
-food(ixobrychus_exilis,					    fish).
-food(ardea_herodias,					      fish).
-food(ardea_alba,						        fish).
-food(egretta_thula,						      fish).
-food(egretta_caerulea,					    fish).
-food(egretta_tricolor,					    fish).
-food(egretta_rufescens,					    fish).
-food(bubulcus_ibis,						      insects).
-food(butorides_virescens,				    fish).
-food(nycticorax_nycticorax,				  fish).
-food(nyctanassa_violacea,				    insects).
-food(eudocimus_albus,					      insects).
-food(plegadis_falcinellus,				  insects).
-food(plegadis_chihi,					      insects).
-food(platalea_ajaja,					      fish).
+food(pelecanus_erythrorhynchos,		fish).
+food(pelecanus_occidentalis,	        fish).
+food(botaurus_lentiginosus,		fish).
+food(ixobrychus_exilis,			fish).
+food(ardea_herodias,			fish).
+food(ardea_alba,			fish).
+food(egretta_thula,			fish).
+food(egretta_caerulea,			fish).
+food(egretta_tricolor,			fish).
+food(egretta_rufescens,			fish).
+food(butorides_virescens,		fish).
+food(nycticorax_nycticorax,		fish).
+food(platalea_ajaja,			fish).
+food(bubulcus_ibis,			insects).
+food(nyctanassa_violacea,		insects).
+food(eudocimus_albus,			insects).
+food(plegadis_falcinellus,		insects).
+food(plegadis_chihi,			insects).
 
 
 %Nesting information
 %Possible nesting locations: ground, tree
-nesting(pelecanus_erythrorhynchos,		ground).
-nesting(pelecanus_occidentalis,			  tree).
-nesting(botaurus_lentiginosus,			  ground).
-nesting(ixobrychus_exilis,				    ground).
-nesting(ardea_herodias,					      tree).
-nesting(ardea_alba,						        tree).
-nesting(egretta_thula,					      tree).
-nesting(egretta_caerulea,				      tree).
-nesting(egretta_tricolor,				      tree).
-nesting(egretta_rufescens,				    tree).
-nesting(bubulcus_ibis,					      tree).
-nesting(butorides_virescens,			    tree).
-nesting(nycticorax_nycticorax,		  	tree).
-nesting(nyctanassa_violacea,			    tree).
-nesting(eudocimus_albus,				      tree).
-nesting(plegadis_falcinellus,			    ground).
-nesting(plegadis_chihi,					      ground).
-nesting(platalea_ajaja,					      tree).
+nesting(pelecanus_erythrorhynchos,	ground).
+nesting(botaurus_lentiginosus,		ground).
+nesting(ixobrychus_exilis,		ground).
+nesting(plegadis_falcinellus,		ground).
+nesting(plegadis_chihi,			ground).
+nesting(pelecanus_occidentalis,		tree).
+nesting(ardea_herodias,			tree).
+nesting(ardea_alba,			tree).
+nesting(egretta_thula,			tree).
+nesting(egretta_caerulea,		tree).
+nesting(egretta_tricolor,		tree).
+nesting(egretta_rufescens,		tree).
+nesting(bubulcus_ibis,			tree).
+nesting(butorides_virescens,		tree).
+nesting(nycticorax_nycticorax,		tree).
+nesting(nyctanassa_violacea,		tree).
+nesting(eudocimus_albus,		tree).
+nesting(platalea_ajaja,			tree).
 
 
 
 %Behaviour information
 %Possible behaviours: surfaceDive, aerialDive, stalking, groundForager, probing
-behavior(pelecanus_erythrorhynchos,		surfaceDive).
-behavior(pelecanus_occidentalis,		  aerialDive).
-behavior(botaurus_lentiginosus,			  stalking).
-behavior(ixobrychus_exilis,				    stalking).
-behavior(ardea_herodias,				      stalking).
-behavior(ardea_alba,					        stalking).
-behavior(egretta_thula,					      stalking).
-behavior(egretta_caerulea,				    stalking).
-behavior(egretta_tricolor,				    stalking).
-behavior(egretta_rufescens,				    stalking).
-behavior(bubulcus_ibis,					      groundForager).
-behavior(butorides_virescens,			    stalking).
-behavior(nycticorax_nycticorax,			  stalking).
-behavior(nyctanassa_violacea,			    stalking).
-behavior(eudocimus_albus,				      probing).
-behavior(plegadis_falcinellus,			  groundForager).
-behavior(plegadis_chihi,				      probing).
-behavior(platalea_ajaja,				      probing).
-
+behavior(pelecanus_erythrorhynchos,	surfaceDive).
+behavior(pelecanus_occidentalis,	aerialDive).
+behavior(botaurus_lentiginosus,		stalking).
+behavior(ixobrychus_exilis,		stalking).
+behavior(ardea_herodias,		stalking).
+behavior(ardea_alba,			stalking).
+behavior(egretta_thula,			stalking).
+behavior(egretta_caerulea,		stalking).
+behavior(egretta_tricolor,		stalking).
+behavior(egretta_rufescens,		stalking).
+behavior(butorides_virescens,		stalking).
+behavior(nycticorax_nycticorax,		stalking).
+behavior(nyctanassa_violacea,		stalking).
+behavior(eudocimus_albus,		probing).
+behavior(plegadis_chihi,		probing).
+behavior(platalea_ajaja,		probing).
+behavior(bubulcus_ibis,			groundForager).
+behavior(plegadis_falcinellus,		groundForager).
 
 
 %Conservation information:
@@ -233,7 +232,6 @@ conservation(ardea_alba,			lc).
 conservation(egretta_thula,			lc).
 conservation(egretta_caerulea,			lc).
 conservation(egretta_tricolor,			lc).
-conservation(egretta_rufescens,			nt).
 conservation(bubulcus_ibis,			lc).
 conservation(butorides_virescens,		lc).
 conservation(nycticorax_nycticorax,		lc).
@@ -242,3 +240,4 @@ conservation(eudocimus_albus,			lc).
 conservation(plegadis_falcinellus,		lc).
 conservation(plegadis_chihi,			lc).
 conservation(platalea_ajaja,			lc).
+conservation(egretta_rufescens,			nt).
